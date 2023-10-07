2013134d119581bec2f8555ea7680602631bf011
Yash Patil <yashpatil.uwu@gmail.com>
2023-10-07 19:01:01 +0530

    map bounds

--- public/js/map.js
+++ public/js/map.js
@@ -1,10 +1,19 @@
 
 document.addEventListener("DOMContentLoaded", () => {
-    const map = L.map('map').setView([25.3548, 51.1839], 2)
+    const map = L.map('map', {
+        center: [25.3548, 51.1839],
+        maxBounds: [
+        [90, -180], 
+        [-90, 180], 
+    ],
+    maxBoundsViscosity: 1.0
+
+    }).setView([25.3548, 51.1839], 1)
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 3,
+        maxZoom: 3, minZoom:2
     }).addTo(map)
+    
 
     const waypoints = [
         {coordinates: [33.1376, 81.8262], id: 1},
